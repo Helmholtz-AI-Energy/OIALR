5dff2b49c926908db1a75f304dd5357694ea1132
coquelin77 <daniel.coquelin@gmail.com>
2023-03-29 09:57:32 +0200

    added log0 function to only log on rank 0, moved orthogonal mat generation to utils

--- src/madonna/utils/utils.py
+++ src/madonna/utils/utils.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import logging
 from functools import wraps
-from typing import Any, Callable
-import torch.nn as nn
+from typing import Any, Callable, List, Tuple, Union
 
+import torch
 import torch.distributed as dist
+import torch.nn as nn
 
 
 def only_on_rank_n(run_on_rank: int = 0):
@@ -22,7 +24,8 @@ def only_on_rank_n(run_on_rank: int = 0):
 
     def rank_zero_only(fn: Callable) -> Callable:
         """Wrap a function to call internal function only in rank zero.
-        Function that can be used as a decorator to enable a function/method being called only on global rank 0.
+        Function that can be used as a decorator to enable a function/method
+        being called only on global rank 0.
         """
 
         @wraps(fn)
@@ -48,8 +51,72 @@ def print0(*args, sep=" ", end="\n", file=None):
         print(*args, sep=sep, end=end, file=file)
 
 
+@only_on_rank_n(0)
+def log0(msg, level, logger=None, *args, **kwargs):
+    if logger is None:
+        logger = logging.getLogger(__name__)
+    getattr(logger, level)(msg, *args, **kwargs)
+
+
 def change_batchnorm_tracking(model: nn.Module, tracking=False):
     for child in model.children():
         if hasattr(child, "track_running_stats"):
             child.track_running_stats = tracking
         change_batchnorm_tracking(child, tracking)
+
+
+def roll_orthogonal_values(
+    shape: Union[Tuple, List, torch.Size],
+    dtype: torch.dtype,
+    device: torch.device,
+    scale_factor: float = 0.1,
+):
+    """
+    References
+    ----------
+    .. [1] F. Mezzadri, "How to generate random matrices from the classical
+       compact groups", :arXiv:`math-ph/0609050v2`.
+
+    Parameters
+    ----------
+    shape
+    dtype
+    device
+    scale_factor
+
+    Returns
+    -------
+
+    """
+    # roll new "gradients" to search the local area
+    # the gradients are orthogonal matrices, normally between -1 and 1
+    # TODO: since the parameters are typically on the same order of magnitude, it may be
+    #  needed to scale the gradients. TBD.
+    if len(shape) == 1:
+        return torch.randn(shape, dtype=dtype, device=device) * scale_factor
+
+    z = torch.randn(shape, dtype=dtype, device=device)
+    if shape[-1] > shape[-2]:
+        # need to transpose? or just do complete, then slice of the bad bits
+        if len(shape) > 2:
+            hold = torch.arange(len(shape) - 2)
+            x_perm = z.permute(*hold, -1, -2)
+            q, r = torch.linalg.qr(x_perm, mode="reduced")
+            d = r.diagonal()
+            ret = q * (d / d.abs()).unsqueeze(-2)
+            ret = ret[..., : x_perm.shape[-1]]
+            ret = ret.permute(*hold, -1, -2)
+        else:
+            x_perm = z.permute(-1, -2)
+            q, r = torch.linalg.qr(x_perm, mode="reduced")
+            d = r.diagonal()
+            # print('h', q.shape, d.shape)
+            ret = q * (d / d.abs()).unsqueeze(-2)
+            ret = ret[..., : x_perm.shape[-1]]
+            ret = ret.permute(-1, -2)
+    else:
+        z = torch.randn(shape, dtype=dtype, device=device)
+        q, r = torch.linalg.qr(z, mode="reduced")
+        d = r.diagonal()
+        ret = q * (d / d.abs()).unsqueeze(-2)
+    return ret * scale_factor
