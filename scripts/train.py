fc9a0d1bb06acd064cf4f9c62c532580fe2995b6
coquelin77 <daniel.coquelin@gmail.com>
2023-03-31 11:05:26 +0200

    mostly debug and cleaning

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,7 @@ import os
 
 import hydra
 import mlflow
+from mpi4py import MPI
 from omegaconf import DictConfig, OmegaConf, open_dict
 from rich.pretty import pprint
 
@@ -31,16 +32,15 @@ def main(config: DictConfig):
     # from madonna.training_pipeline import train
     # Applies optional utilities
     # utils.extras(config)
-    pprint(config)
+    # pprint(config)
     #
     # optimizer = madonna.utils.get_optimizer(config, model)
     # sch, warmup = madonna.utils.get_lr_schedules(config, optimizer, 10)
-    log.info("comm init")
+    pprint("before comm init")
     rank, size = utils.comm.init_and_set_config_rank_size(config)
     with open_dict(config):
         config["world_size"] = size
         config["rank"] = rank
-    log.info(f"Rank: {rank}, world size: {size}")
 
     if config.training.trainer == "slime":
         fn = madonna.trainers.slime_trainer.main
@@ -51,9 +51,11 @@ def main(config: DictConfig):
     else:
         raise ValueError(f"unknonw trainer: {config.trainer.trainer}")
 
+    if rank == 0:
+        pprint(dict(config))
+
     if rank == 0 and not config.skip_tracking:
         _ = utils.tracking.setup_mlflow(config, verbose=False)
-        pprint(config)
 
         # run_id -> adaptive needs to be unique, roll random int?
         # run_name = f"" f"full-rank-everybatch-{os.environ['SLURM_JOBID']}"
@@ -61,15 +63,21 @@ def main(config: DictConfig):
             mlflow.log_param("Slurm jobid", os.environ["SLURM_JOBID"])
             run_name = f"{config['name']}-" + run.info.run_name
             mlflow.set_tag("mlflow.runName", run_name)
+
             # print("run_name:", run_name)
             # print("tracking uri:", mlflow.get_tracking_uri())
             # print("artifact uri:", mlflow.get_artifact_uri())
+            log.info(f"Rank: {rank}, world size: {size}")
+
             log.info(f"run_name: {run_name}")
             log.info(f"tracking uri: {mlflow.get_tracking_uri()}")
             log.info(f"artifact uri: {mlflow.get_artifact_uri()}")
             madonna.utils.tracking.log_config(config)
             # hydra.utils.call(config.training.script, config)
             fn(config)
+    elif not config.skip_tracking:
+        # receive the name for the logs
+        pass
     else:
         # hydra.utils.call(config.training.script, config)
         fn(config)
